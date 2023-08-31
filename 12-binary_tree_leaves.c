9e03161a731508e7bc4640c0ca70f37130700b7b
kazibweyassin <kazibweusama@gmail.com>
2023-08-31 01:19:16 -0700

    error

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,21 +1,31 @@
 #include "binary_trees.h"
 
+size_t s_helper(const binary_tree_t *tree);
+
 /**
  * binary_tree_leaves - Count the number of leaf nodes in a binary tree.
  * @tree: Pointer to the root node of the tree to count leaves.
- *
  * Return: The number of leaf nodes in the binary tree, or 0 if tree is NULL.
  */
-size_t binary_tree_leaves(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
+	return (s_helper(tree));
+}
 
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
+/**
+ * s_helper - recursive helper with accumulator
+ * @tree: Pointer to the root node of the tree to count leaves.
+ * Return: The number of leaf nodes in the binary tree, or 0 if tree is NULL.
+ */
+size_t s_helper(const binary_tree_t *tree)
+{
+	size_t a, b;
 
-    size_t left_leaves = binary_tree_leaves(tree->left);
-    size_t right_leaves = binary_tree_leaves(tree->right);
-
-    return (left_leaves + right_leaves);
+	if (tree == NULL)
+		return (0);
+	a = s_helper(tree->left);
+	b = s_helper(tree->right);
+	return (a + b + 1);
 }
