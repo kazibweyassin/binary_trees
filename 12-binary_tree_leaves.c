4f669a6f9f5669b1fc2573cb5e6125527bdf8080
kazibweyassin <kazibweusama@gmail.com>
2023-08-31 01:27:10 -0700

    number of nodes

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,31 +1,33 @@
 #include "binary_trees.h"
 
-size_t s_helper(const binary_tree_t *tree);
+size_t leaves_helper(const binary_tree_t *tree);
 
 /**
  * binary_tree_leaves - Count the number of leaf nodes in a binary tree.
  * @tree: Pointer to the root node of the tree to count leaves.
  * Return: The number of leaf nodes in the binary tree, or 0 if tree is NULL.
  */
-size_t binary_tree_size(const binary_tree_t *tree)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	return (s_helper(tree));
+    if (tree == NULL)
+        return (0);
+    return (leaves_helper(tree));
 }
 
 /**
- * s_helper - recursive helper with accumulator
+ * leaves_helper - Recursive helper function to count leaf nodes.
  * @tree: Pointer to the root node of the tree to count leaves.
  * Return: The number of leaf nodes in the binary tree, or 0 if tree is NULL.
  */
-size_t s_helper(const binary_tree_t *tree)
+size_t leaves_helper(const binary_tree_t *tree)
 {
-	size_t a, b;
+    size_t a, b;
 
-	if (tree == NULL)
-		return (0);
-	a = s_helper(tree->left);
-	b = s_helper(tree->right);
-	return (a + b + 1);
+    if (tree == NULL)
+        return (0);
+    if (tree->left == NULL && tree->right == NULL)
+        return (1); // This node is a leaf
+    a = leaves_helper(tree->left);
+    b = leaves_helper(tree->right);
+    return (a + b);
 }
